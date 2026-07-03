611205fec46e8664267ac016654d56d5c4f12388
Jash-k <dangerdreamer2010@gmail.com>
2026-07-03 18:57:39 +0530

    fix player

---
+++ config.js
@@ -0,0 +1,9 @@
+
+window.JASH_CONFIG = {
+    // Your Stremio Addon Base URL
+    BASE_URL: "https://stremiotesting-production.up.railway.app",
+    
+    // Optional: App settings
+    APP_NAME: "JaSH OTT",
+    VERSION: "1.0.0"
+};
